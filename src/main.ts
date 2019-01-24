a5a088c89f971952a0cf0703da7557abaffdaaf2
Celeste <nsznsznjsz@outlook.com>
2019-01-24 23:01:32 +0800

    安装Axios

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI)
+
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
