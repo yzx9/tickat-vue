80f68627fe209fe69e1822492c07395e149456b3
Celeste <nsznsznjsz@outlook.com>
2019-01-30 16:09:30 +0800

    配置404页面

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import Home from './views/Home.vue';
 import About from '@/views/About.vue';
 import Login from '@/views/Login.vue';
 import Signup from '@/views/Signup.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(Router);
 
@@ -33,6 +34,16 @@ export default new Router({
       path: '/signup',
       name: 'signup',
       component: Signup,
+    },
+    {
+      path: '/404',
+      alias: '/notfound',
+      name: 'notfound',
+      component: NotFound,
+    },
+    {
+      path: '*',
+      redirect: '/404',
     },
   ],
 });
---
+++ src/views/NotFound.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="center">
+    <p>404 Not Found</p>
+    <el-button @click="goHome">主页</el-button>
+    <el-button @click="goBack">返回</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class NotFound extends Vue {
+  public goHome() {
+    this.$router.push('/');
+  }
+  public goBack() {
+    this.$router.go(-1);
+  }
+}
+</script>
+
+<style lang="less">
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 500px;
+  width: 100%;
+}
+</style>
