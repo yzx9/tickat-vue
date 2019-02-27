149820adf36472b7e9840f4b4dde30fe3f27412c
Celeste <nsznsznjsz@outlook.com>
2019-02-27 17:26:34 +0800

    refactor: router

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 import { store } from '@/store'
-// router
 import router from '@/router'
-import '@/router/navigationGuards'
 // axios
 import axios from 'axios'
 // Font Awesome
--- src/router/navigationGuards.ts
+++ src/router/navigationGuards.ts
@@ -1,4 +1,4 @@
-import router from '@/router'
+import router from '@/router/router'
 import Vuex, { Store as VuexStore } from 'vuex'
 import { store } from '@/store'
 
